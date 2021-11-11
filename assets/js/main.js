bd621d5e411a54712fc138a2d06188930b8ae02d
Thedragon-15 <rafael.r191211@gmail.com>
2021-11-11 14:57:27 -0400

    Refactorizando Codigo y reparando errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,50 @@
 const navMenu = document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-if(navToggle){
-    navToggle.addEventListener('click' , () => {
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-if (navClose){
-    navClose.addEventListener('click' , () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
 /* ============ REMOVE MENU MOBILE  ============= */
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
     /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* ============ ACCORDION SKILLS  ============= */
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+    function toggleSkills(){
+        let itemClass = this.parentNode.className
+
+
+        for(i = 0; i < skillsContent.length; i++){
+            skillsContent[i].className = 'skills__content skills__close'
+        }
+        if(itemClass === 'skills__content skills__close'){
+            this.parentNode.className = 'skills__content skills__open'
+        }
+    }
+
+    skillsHeader.forEach((el) =>{
+        el.addEventListener('click' , toggleSkills)
+    })
+
+
+
