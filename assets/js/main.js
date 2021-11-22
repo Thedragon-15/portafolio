ac34748b124bd0b2557d6d7f67f9e30b7d39a4a8
Thedragon-15 <rafael.r191211@gmail.com>
2021-11-22 10:45:36 -0400

    agregando qualification con Detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,5 +46,24 @@ const skillsContent = document.getElementsByClassName('skills__content'),
         el.addEventListener('click' , toggleSkills)
     })
 
+    /* ============== QUALIFICATION TABS=============== */
 
+    const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll(['data-content'])
+
+    tabs.forEach(tab=>{
+        tab.addEventListener('click', () =>{
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContents.forEach(tabContent =>{
+                tabContents.classList.remove('qualification__active')
+            })
+            target.classList.add('qualification__active')
+
+            tab.forEach(tab => {
+                tab.classList.remove('qualification__active')
+            })
+            tab.classList.add('qualification__active')
+        })
+    })
 
