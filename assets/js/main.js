76381ad4b46d8c882dace65fe1220ee65c2831b3
Diego Palacios <rafael.r191211@gmail.com>
2022-01-17 15:21:05 -0400

    add darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,7 +145,7 @@ function scrollActive(){
 }
 window.addEventListener('scroll' , scrollActive)
  
-/*=========== SCROLL SECTIONS ACTIVE LINK=============*/
+/*=========== backgraund=============*/
 function scrollHeader(){
   const nav = document.getElementById('header')
   if(this.scrollY >=  80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
