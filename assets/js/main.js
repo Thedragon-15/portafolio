b0104fa2505998980308f4ebfe31160f03ff613a
diego palacios <rafael.r191211@gmail.com>
2022-02-18 12:10:57 -0400

    refactorizando biografia Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,17 +26,17 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* ============ ACCORDION SKILLS  ============= */
 
-const skillsContent = document.getElementsByClassName("skills__content"),
-  skillsHeader = document.querySelectorAll(".skills__header");
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header');
 
 function toggleSkills() {
-  let itemClass = this.parentNode.className;
+  let  itemClass = this.parentNode.className;
 
   for (i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = "skills__content skills__close";
+    skillsContent[i].className = 'skills__content skills__close';
   }
-  if (itemClass === "skills__content skills__close") {
-    this.parentNode.className = "skills__content skills__open";
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
   }
 }
 
@@ -45,23 +45,23 @@ skillsHeader.forEach((el) => {
 });
 
 /* ============== QUALIFICATION TABS=============== */
-
 const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll(["data-content"]);
+  tabContents = document.querySelectorAll("[data-content]")
 
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
+    const target = document.querySelector(tab.dataset.target)
 
-    tabContents.forEach((tabContent) => {
-      tabContents.classList.remove("qualification__active");
-    });
-    target.classList.add("qualification__active");
+    tabContents.forEach(tabContents =>{
+      tabContents.classList.remove('qualification__active')
+    })
+    
+     target.classList.add('qualification__active')
 
-    tab.forEach((tab) => {
-      tab.classList.remove("qualification__active");
-    });
-    tab.classList.add("qualification__active");
+     tab.forEach(tab =>{
+       tab.classList.remove('qualification__active')
+     })
+     tab.classList.add('qualification__active')
   });
 });
 /*=========== SERVICES MODAL =============*/
